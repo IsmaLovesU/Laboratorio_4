a275db5ec51cf33407732b2efd28ee590a8c09bf
JorgeVilledaS <vil24932@uvg.edu.gt>
2024-11-08 12:03:07 -0600

    Nomás estandarizamos más algunos nombres

--- ClaseA.java
+++ ClaseA.java
@@ -1,7 +1,7 @@
 import java.util.Date;
 
 public interface ClaseA {
-    public boolean cambiar(String decision);
+    public String cambiar(Boolean sonido);
     public String planificarViajes(Date inicio, Date fin, String lugar_inicio, String lugar_final);
     
 
