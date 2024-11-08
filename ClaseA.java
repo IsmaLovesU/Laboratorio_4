20db57b64dde301c807d3821420ae3ba6ae051b3
JorgeVilledaS <vil24932@uvg.edu.gt>
2024-11-08 11:43:39 -0600

    Interfaz

---
+++ ClaseA.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public interface ClaseA {
+    public boolean cambiar(String decision);
+    public String planificarViajes(Date inicio, Date fin, String lugar_inicio, String lugar_final);
+    
+
+
+}
