47cb1ed74f20de4ed808448ca3ccb6feec3a3a5e
Andrés Ismalej <andesteisma@gmail.com>
2024-11-08 21:43:43 -0600

    Actualización

--- ClaseA.java
+++ ClaseA.java
@@ -3,7 +3,4 @@ import java.util.Date;
 public interface ClaseA {
     public String cambiar(Boolean sonido);
     public String planificarViajes(Date inicio, Date fin, String lugar_inicio, String lugar_final);
-    
-
-
 }
