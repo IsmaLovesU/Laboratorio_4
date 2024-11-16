1249432b99f67e31275d42c852c24a994e6c1832
JuanGualim <gua24852@uvg.edu.gt>
2024-11-15 21:09:51 -0600

    Comentarios de la clase Emisora

--- Emisora.java
+++ Emisora.java
@@ -1,15 +1,54 @@
+/**
+ * Clase que representa una emisora de radio con modulación y estación.
+ */
 public class Emisora {
-    private String modulacion;
-    private double estacion;
+    private String modulacion; // Tipo de modulación de la emisora (AM o FM).
+    private double estacion;   // Frecuencia de la emisora.
 
+    /**
+     * Constructor que inicializa una emisora con su modulación y estación.
+     * 
+     * @param modulacion El tipo de modulación de la emisora (AM o FM).
+     * @param estacion La frecuencia de la emisora.
+     */
     public Emisora(String modulacion, double estacion) {
         this.modulacion = modulacion;
         this.estacion = estacion;
     }
 
-    public String getModulacion() { return modulacion; }
-    public void setModulacion(String modulacion) { this.modulacion = modulacion; }
+    /**
+     * Obtiene el tipo de modulación de la emisora.
+     * 
+     * @return El tipo de modulación (AM o FM).
+     */
+    public String getModulacion() {
+        return modulacion;
+    }
 
-    public double getEstacion() { return estacion; }
-    public void setEstacion(double estacion) { this.estacion = estacion; }
+    /**
+     * Establece el tipo de modulación de la emisora.
+     * 
+     * @param modulacion El nuevo tipo de modulación (AM o FM).
+     */
+    public void setModulacion(String modulacion) {
+        this.modulacion = modulacion;
+    }
+
+    /**
+     * Obtiene la frecuencia de la emisora.
+     * 
+     * @return La frecuencia de la emisora.
+     */
+    public double getEstacion() {
+        return estacion;
+    }
+
+    /**
+     * Establece la frecuencia de la emisora.
+     * 
+     * @param estacion La nueva frecuencia de la emisora.
+     */
+    public void setEstacion(double estacion) {
+        this.estacion = estacion;
+    }
 }
